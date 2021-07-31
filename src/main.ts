488406a2fff6d2e52c4b55383cd111ef4a49ab9d
Layton Whiteley <layton.whiteley@gmail.com>
2021-07-31 07:12:04 +0200

    chore: export extra services

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,7 @@ export * from "./UnsplashCollections";
 export * from "./UnsplashCollectionPhotos";
 export * from "./UnsplashTopics";
 export * from "./UnsplashTopicPhotos";
+export * from "./UnsplashUserCollections";
+export * from "./UnsplashUserLikes";
+export * from "./UnsplashUserPhotos";
+export * from "./UnsplashUsers";
